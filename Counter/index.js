4b95367a4e6162a75f86f7a8017f49b906ccfd38
crisncris0000 <53241124+crisncris0000@users.noreply.github.com>
2021-07-02 16:24:07 -0400

    Add files via upload

---
+++ Counter/index.js
@@ -0,0 +1,28 @@
+document.querySelector("#Increase").onclick = function()
+{
+    
+    let h1 = document.querySelector("#Counter");
+    h1.innerHTML ++;
+
+}
+
+document.querySelector("#Decrease").onclick = function()
+{
+    
+    let h1 = document.querySelector("#Counter");
+    h1.innerHTML --;
+
+}
+
+document.querySelector("#Reset").onclick = function()
+{
+    
+    let h1 = document.querySelector("#Counter");
+    h1.innerHTML = 0;
+
+}
+
+
+
+document.querySelector
+
