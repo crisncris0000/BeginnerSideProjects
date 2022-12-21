70a9f52db08fa9ec538a5291d0b6a054a5674afb
Christopher Rivera <christopherrivera384@gmail.com>
2022-12-20 20:41:04 -0500

    Added setters/getters to Entity classes

--- ShoppingSite/src/main/java/com/springboot/ShoppingSite/Entity/Authority.java
+++ ShoppingSite/src/main/java/com/springboot/ShoppingSite/Entity/Authority.java
@@ -13,4 +13,27 @@ public class Authority {
     @Column(name = "authority")
     private String role;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Authority{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
--- ShoppingSite/src/main/java/com/springboot/ShoppingSite/Entity/User.java
+++ ShoppingSite/src/main/java/com/springboot/ShoppingSite/Entity/User.java
@@ -13,6 +13,43 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @JoinColumn(name = "")
+    @OneToOne(cascade = {CascadeType.DETACH,
+                         CascadeType.MERGE,
+                         CascadeType.REFRESH,
+                         CascadeType.PERSIST})
+    @JoinColumn(name = "authorities")
     private Authority authority;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Authority getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(Authority authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", authority=" + authority +
+                '}';
+    }
 }
