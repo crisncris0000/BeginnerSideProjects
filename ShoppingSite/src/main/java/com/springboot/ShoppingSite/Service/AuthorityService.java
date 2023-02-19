d5ab281508ff8e2ded6676cb6e13dd69f33f63b9
Christopher Rivera <53241124+crisncris0000@users.noreply.github.com>
2023-02-19 15:35:47 -0500

    Delete ShoppingSite directory

--- ShoppingSite/src/main/java/com/springboot/ShoppingSite/Service/AuthorityService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.ShoppingSite.Service;
-
-import com.springboot.ShoppingSite.Entity.Authority;
-
-public interface AuthorityService {
-
-    public Authority findAuthorityById(int id);
-
-}
